c68dde83191e179632ee55d4cd1e9dadee52cc60
Ankit Majee <ankitmajee.05@gmail.com>
2023-06-13 23:02:30 +0530

    1st Commit

---
+++ Java/InterfaceShape.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+interface Shape {
+    double calculatePerimeter();
+}
+
+class Square implements Shape {
+    private double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    public double calculatePerimeter() {
+        return 4 * side;
+    }
+}
+
+class Circle implements Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double calculatePerimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
+
+public class InterfaceShape {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Side of Square: ");
+        int s = sc.nextInt();
+        Square square = new Square(s);
+
+        System.out.print("Enter radius of Circle: ");
+        int radius = sc.nextInt();
+        Circle circle = new Circle(radius);
+
+        double squarePerimeter = square.calculatePerimeter();
+        double circlePerimeter = circle.calculatePerimeter();
+
+        System.out.println("Square Perimeter: " + squarePerimeter);
+        System.out.println("Circle Perimeter: " + circlePerimeter);
+    }
+}
